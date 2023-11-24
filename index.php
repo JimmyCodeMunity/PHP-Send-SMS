9fdd52e46ce49ca185817e8e71095d8f26531a16
Jimmy <96749667+JimmyCodeMunity@users.noreply.github.com>
2023-11-24 11:22:29 +0300

    Update index.php

--- index.php
+++ index.php
@@ -2,8 +2,8 @@
 require 'vendor/autoload.php';
 use AfricasTalking\SDK\AfricasTalking;
 
-$username = 'fibororeminder'; // use 'sandbox' for development in the test environment
-$apiKey   = '541ad11c4896b6505fa4507a1b55699836d9f268a57e5b1c2eb3cde032de39f7'; // use your sandbox app API key for development in the test environment
+$username = ''; // use 'sandbox' for development in the test environment
+$apiKey   = ''; // use your sandbox app API key for development in the test environment
 $AT       = new AfricasTalking($username, $apiKey);
 
 // Get one of the services
